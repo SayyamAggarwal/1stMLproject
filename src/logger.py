d807dde096593572fea8a4e53a6f6823760fd4e7
SayyamAggarwal <sayyamaggarwal2005@gmail.com>
2025-07-21 22:39:55 +0530

    added notebooks

--- src/logger.py
+++ src/logger.py
@@ -1,22 +1,16 @@
 import logging
 import os
-from datetime import datetime 
+from datetime import datetime
 
-LOG_FILE = f"{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.log"
-logs_path = os.path.join(os.getcwd(),'Logs', LOG_FILE)
-os.makedirs(logs_path, exist_ok=True)
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
 
-
-LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE) 
-
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format='[%(asctime)s] - %(lineno)s - %(levelname)s - %(message)s',
-    level= logging.INFO,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
 )
 
-
-if __name__ == "__main__":
-    logging.info("Logging has been set up successfully.")
-    print(f"Logs will be saved to {LOG_FILE_PATH}")
